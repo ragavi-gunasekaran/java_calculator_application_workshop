0c47a7b4beb78baee0b9197a359aa4fa9f3734df
Ragavi Gunasekaran <ragavigunasekaran@gmail.com>
2026-02-26 10:16:58 +0100

    Added scanner class and added the console menu to get the input from user

--- src/main/java/lexicon/workshop/Main.java
+++ src/main/java/lexicon/workshop/Main.java
@@ -1,10 +1,33 @@
 package lexicon.workshop;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     static void main() {
 
-        }
+        // To get console based input from the user
+        Scanner scan = new Scanner(System.in);
+
+        IO.println("Welcome to Calculator Application");
+        IO.println("Please give your input to perform any calculations");
+        IO.print("Input number 1 : " );
+        int number1 = scan.nextInt();
+        IO.print("Input number 2 : ");
+        int number2 = scan.nextInt();
+
+
+        IO.println("Please select the below choice of operation which you want to perform for your input entered.");
+        IO.println("1. Addition");
+        IO.println("2. Subtraction");
+        IO.println("3. Multiplication");
+        IO.println("4. Division");
+
+
+
+
+
+
+
     }
-}
+    }
+
